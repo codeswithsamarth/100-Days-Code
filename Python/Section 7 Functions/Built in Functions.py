d2c409e3275cc55588a576405db825e1e1877d71
Samarth <samarthp2727@gmail.com>
2025-06-14 10:18:56 +0530

    First commit

--- Python/Section 7 Functions/Built in Functions.py
+++ Python/Section 7 Functions/Built in Functions.py
@@ -16,3 +16,47 @@ print(bool(value))
 byte = bytes(65)
 print((byte))
 
+def my_fun():
+    pass
+print(callable(my_fun()))
+
+num = 97
+print(chr(num))
+
+# compile functions compile source code
+
+code = compile("x = 10*10","sam.py","exec")
+exec(code)
+print(x)
+
+number = complex(2,3)
+print(number)
+
+# dir prints the list of attribute of an object
+print(dir([]))
+
+number,number2 = divmod(2,7)
+print(number,number2)
+
+# enumerate is used to return index and value
+my_list = ["Samarth","Sanket","Hashit"]
+for i,name in enumerate(my_list):
+    print(i)
+
+x = 5
+result = eval('x+2')
+print(result)
+
+print(hash('hello'))
+
+help(str)
+
+print(hex(233))
+
+print(id('hello'))
+
+print(isinstance(5,int))
+
+print(len('hello'))
+
+print(max([1,2,3]))
